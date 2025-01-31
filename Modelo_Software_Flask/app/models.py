178407589bcbf4aed37e77c26fa6628253b73c26
GabrielSilva87 <gabri2012mi@gmail.com>
2025-01-30 21:34:42 -0300

    Atualizar o app

---
+++ Modelo_Software_Flask/app/models.py
@@ -0,0 +1,16 @@
+from app import db, login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.Text, nullable=True)
